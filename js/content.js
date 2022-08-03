1e309868d8f338b1a0eade618564e8036c86bbf1
FANDI <851453073@qq.com>
2022-08-03 13:48:36 +0800

    首次提交

--- js/content.js
+++ js/content.js
@@ -14,32 +14,31 @@ let downloadCount = 0;
 $(document).ready(function () {
     let deckDom = $("#desc_text");
     if (deckDom.length) {
-        let $navMobi = $("#nav_mobi");
-        $navMobi.trigger("click");
-        total = $("#quota_login").text().match(/\d+/);
-
-        let table = $(".book_list");
-        let tr = $(table[1]).find("tr")
-        tr[0].remove();
-        tr[1].remove();
-
-        let innerHTML = "<p style='font-size: 20px; color:#F56C6C;position:fixed;top:100px;left:10px;background-color: #EBEEF5;padding: 10px;border: 1px solid #DCDFE6'>"
-        innerHTML += "批量下载 <br>(已选 / 剩余 : <span id='use'>0 M</span> / " + total + " M)";
-        innerHTML += "<br><span style='font-size: 12px'>选中后点击下载即可批量下载</span><br>";
-        innerHTML += "<span style='font-size: 12px'>注意不要超过剩余额度</span><br>";
-        innerHTML += "<span style='font-size: 12px'>非VIP用户下载有数量限制，一次不建议下载太多</span><br>";
-        innerHTML += "<span style='font-size: 12px'>每30S下载一个，不要关闭本页面。</span><br>";
-        innerHTML += "<span id='warn' style='font-size: 12px;display: none'>额度不够了哦!</span><br>";
-        innerHTML += "<button style='margin-left: 10px;margin-right: 10px' id='allSelect'> 全选 </button>";
-        innerHTML += "<button style='margin-right: 10px' id='allNoSelect'> 全不选 </button>";
-        innerHTML += "<button id='download'> 下载 </button>";
-        innerHTML += "</p>"
-        deckDom.html(deckDom.html() + innerHTML);
-
         setTimeout(() => {
+            $("#nav_mobi").trigger("click");
+            total = $("#quota_login").text().match(/\d+/);
+
+            let table = $(".book_list");
+            let tr = $(table[1]).find("tr")
+            tr[0].remove();
+            tr[1].remove();
+
+            let innerHTML = "<p style='font-size: 20px; color:#F56C6C;position:fixed;top:100px;left:10px;background-color: #EBEEF5;padding: 10px;border: 1px solid #DCDFE6'>"
+            innerHTML += "批量下载 <br>(已选 / 剩余 : <span id='use'>0 M</span> / " + total + " M)";
+            innerHTML += "<br><span style='font-size: 12px'>选中后点击下载即可批量下载</span><br>";
+            innerHTML += "<span style='font-size: 12px'>注意不要超过剩余额度</span><br>";
+            innerHTML += "<span style='font-size: 12px'>非VIP用户下载有数量限制，一次不建议下载太多</span><br>";
+            innerHTML += "<span style='font-size: 12px'>每30S下载一个，不要关闭本页面。</span><br>";
+            innerHTML += "<span id='warn' style='font-size: 12px;display: none'>额度不够了哦!</span><br>";
+            innerHTML += "<button style='margin-left: 10px;margin-right: 10px' id='allSelect'> 全选 </button>";
+            innerHTML += "<button style='margin-right: 10px' id='allNoSelect'> 全不选 </button>";
+            innerHTML += "<button id='download'> 下载 </button>";
+            innerHTML += "</p>"
+            deckDom.html(deckDom.html() + innerHTML);
+
             $("input:checkbox").each(function () {
                 $(this).click(function () {
-                    let mobiCheck = $navMobi.hasClass("tab_check");
+                    let mobiCheck = $("#nav_mobi").hasClass("tab_check");
                     let epubCheck = $("#nav_epub").hasClass("tab_check");
                     if (!mobiCheck && !epubCheck) {
                         alert("请点击下载选项卡")
@@ -48,52 +47,52 @@ $(document).ready(function () {
                     updateUse();
                 });
             })
-        }, 500);
 
-        $('#allSelect').on('click', function () {
-            let mobiCheck = $navMobi.hasClass("tab_check");
-            let epubCheck = $("#nav_epub").hasClass("tab_check");
-            if (!mobiCheck && !epubCheck) {
-                alert("请点击下载选项卡")
-                return
-            }
-            $("input:checkbox").attr("checked", "true");
-            updateUse();
-        });
-
-        $('#allNoSelect').on('click', function () {
-            let mobiCheck = $navMobi.hasClass("tab_check");
-            let epubCheck = $("#nav_epub").hasClass("tab_check");
-            if (!mobiCheck && !epubCheck) {
-                alert("请点击下载选项卡")
-                return
-            }
-            $("input:checkbox").removeAttr("checked");
-            updateUse()
-        });
-
-        $('#download').on('click', function () {
-            let mobiCheck = $navMobi.hasClass("tab_check");
-            let epubCheck = $("#nav_epub").hasClass("tab_check");
-            if (!mobiCheck && !epubCheck) {
-                alert("请点击下载选项卡")
-                return
-            }
-            let type = 1;
-            if (epubCheck) {
-                type = 2;
-            }
-            if (downloadCount > 0) {
-                alert("当前正在下载，不要重复点击！")
-                return;
-            }
-            let checkVolDom = $("input[name='checkbox_vol']:checked");
-            downloadCount = checkVolDom.length;
-            let count = 0;
-            checkVolDom.each(function () {
-                download($(this).val(), 30000 * count++, type);
+            $('#allSelect').on('click', function () {
+                let mobiCheck = $("#nav_mobi").hasClass("tab_check");
+                let epubCheck = $("#nav_epub").hasClass("tab_check");
+                if (!mobiCheck && !epubCheck) {
+                    alert("请点击下载选项卡")
+                    return
+                }
+                $("input:checkbox").attr("checked", "true");
+                updateUse();
             });
-        });
+
+            $('#allNoSelect').on('click', function () {
+                let mobiCheck = $("#nav_mobi").hasClass("tab_check");
+                let epubCheck = $("#nav_epub").hasClass("tab_check");
+                if (!mobiCheck && !epubCheck) {
+                    alert("请点击下载选项卡")
+                    return
+                }
+                $("input:checkbox").removeAttr("checked");
+                updateUse()
+            });
+
+            $('#download').on('click', function () {
+                let mobiCheck = $("#nav_mobi").hasClass("tab_check");
+                let epubCheck = $("#nav_epub").hasClass("tab_check");
+                if (!mobiCheck && !epubCheck) {
+                    alert("请点击下载选项卡")
+                    return
+                }
+                let type = 1;
+                if (epubCheck) {
+                    type = 2;
+                }
+                if (downloadCount > 0) {
+                    alert("当前正在下载，不要重复点击！")
+                    return;
+                }
+                let checkVolDom = $("input[name='checkbox_vol']:checked");
+                downloadCount = checkVolDom.length;
+                let count = 0;
+                checkVolDom.each(function () {
+                    download($(this).val(), 30000 * count++, type);
+                });
+            });
+        }, 500);
     }
 });
 
